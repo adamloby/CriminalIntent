33bf57881c56ee83caa49dd531831b9571199051
adamloby <itslubey@gmail.com>
2017-09-12 15:23:07 +1000

    Chap 8 done. v2

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Lubeyy on 12/9/17.
+ */
+
+public class CrimeListFragment extends Fragment {
+    // Nothing yet
+}
